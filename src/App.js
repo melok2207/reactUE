95a252dffb73fd848e7d9f3c00001923398f493b
melok2207 <132545373+melok2207@users.noreply.github.com>
2025-03-16 13:40:29 +0200

    App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ColorBlock from './ColorBlock';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Color Switcher</h1>
+      <ColorBlock />
+    </div>
+  );
+}
+
+export default App;
